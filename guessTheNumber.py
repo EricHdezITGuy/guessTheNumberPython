542ed64ac9c398e74405c8ca98a06292b6830874
EricHdezITGuy <ehernandezm1@ucenfotec.ac.cr>
2022-06-15 11:55:17 -0600

    Programa terminado, ya funciona.

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -4,3 +4,30 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import random
+
+
+def adivina_el_numero(x):
+
+    print("=============================")
+    print(" Bienvenido(a) al juego!")
+    print("=============================")
+    print("Tu meta es adivinar el numero generado por la computadora")
+
+    numero_aleatorio = random.randint(1, x) #Numero aleatorio entre 1 y X.
+
+    prediccion = 0
+
+    while prediccion != numero_aleatorio:
+        prediccion = int(input(f"Adivina un numero entre 1 y {x}: ")) #Estoy usando un F-string
+
+        if prediccion < numero_aleatorio:
+            print("Intenta otra vez. Este numero es muy pequeno")
+        elif prediccion > numero_aleatorio:
+            print("Intenta otra vez. Este numero es muy grande")
+
+    print(f"Felicitaciones! Adivinaste el numero {numero_aleatorio} correctamente") #Estoy usando un F-string
+
+
+adivina_el_numero(10)
